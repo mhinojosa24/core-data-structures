7eeabbee15e930ac6d97151d464629a0133f3653
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-04-04 00:22:25 -0700

    [add] implement factorial iterative function

--- recursion.py
+++ recursion.py
@@ -14,6 +14,7 @@ def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
     # once implemented, change factorial (above) to call factorial_iterative
     # to verify that your iterative implementation passes all tests
+    
     product = 1
     while n > 0:
         product *= n
