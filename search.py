c796e12d51a6278e6c2e53d7438f6e6dc4c83cc9
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-04-08 17:01:54 -0700

    [add] time efficiency

--- search.py
+++ search.py
@@ -1,7 +1,5 @@
 def linear_search(array, item):
     """return the first index of item in array or None if item is not found"""
-    # implement linear_search_iterative and linear_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
     # return linear_search_iterative(array, item)
     return linear_search_recursive(array, item)
 
@@ -27,13 +25,16 @@ def linear_search_recursive(array, item, index=0):
 
 def binary_search(array, item):
     """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
     # return binary_search_iterative(array, item)
     return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
+    '''
+    best time: O(1)
+    worst time: O(log n)
+    space: O(1)
+    '''
 
     low_bound = 0
     high_bound = len(array) -1
