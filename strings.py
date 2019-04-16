eaf575121aa5ad4a2a3e215414401b804676381c
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-04-16 11:15:32 -0700

    [add] implemented insert_at_index method

--- strings.py
+++ strings.py
@@ -77,10 +77,9 @@ def find_all_indexes(text, pattern):
 
 def test_string_algorithms(text, pattern):
     found = contains(text, pattern)
-    # print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
-    # TODO: Uncomment these lines after you implement find_index
+    print('contains({!r}, {!r}) => {}'.format(text, pattern, found))
     index = find_index(text, pattern)
-    # print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
+    print('find_index({!r}, {!r}) => {}'.format(text, pattern, index))
     indexes = find_all_indexes(text, pattern)
     print('find_all_indexes({!r}, {!r}) => {}'.format(text, pattern, indexes))
 
