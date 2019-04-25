b58c258a42b684c4cbf7e54e12a7369160b0984f
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-04-25 16:04:12 -0700

    [add] implemented difference method and test cases for method

--- sets.py
+++ sets.py
@@ -9,13 +9,23 @@ class Set(object):
             for element in elements:
                 self.add(element)
 
+    def __str__(self):
+        """Return a formatted string representation of this hash table."""
+        items = ['{!r}: {!r}'.format(key, val) for key, val in self.hash_set.items()]
+        return '{' + ', '.join(items) + '}'
+
+    def __repr__(self):
+        """Return a string representation of this hash table."""
+        return 'Set({!r})'.format(self.hash_set.items())
+
     def contains(self, element):
         """ return a boolean indicating whether element is in this set """
         return self.hash_set.contains(element)
 
     def add(self, element):
         """ add element to this set, if not present already """
-        return self.hash_set.set(element, value=None)
+        if self.hash_set.contains(element) is False:
+            return self.hash_set.set(element, None)
 
     def remove(self, element):
         """ remove element from this set, if present, or else raise KeyError """
@@ -23,22 +33,51 @@ class Set(object):
 
     def union(self, other_set):
         """ return a new set that is the union of this set and other_set """
-        pass
+        new_set = Set()
+
+        for element in self.hash_set.keys():
+            new_set.add(element)
+
+        for element in other_set.hash_set.keys():
+            new_set.add(element)
+
+        return new_set
 
     def intersection(self, other_set):
         """ return a new set that is the intersection of this set and other_set """
-        pass
+        new_set = Set()
+
+        for element in other_set.hash_set.keys():
+            if element in self.hash_set.keys():
+                new_set.add(element)
+        return new_set
 
     def difference(self, other_set):
         """ return a new set that is the difference of this set and other_set """
-        pass
+        new_set = Set()
+
+        for element in other_set.hash_set.keys():
+            if element not in self.hash_set.keys():
+                new_set.add(element)
+        return new_set
 
     def is_subset(self, other_set):
-        """ return a boolean indicating whether other_set is a subset of this set """
+        """ return a boolean
+        indicating whether other_set is a subset of this set """
         pass
 
-    def test_set(self):
-        set = Set()
+def test_set():
+    s = Set()
+    s.add('I')
+    s.add('E')
+    s.add('Y')
+    print('set: {}'.format(s))
+    s.remove('Y')
+    print('set: {}'.format(s))
+    set = Set(['k', 'G', 'T'])
+    other_set = Set(['K', 'T', 'R'])
+    new_set = set.union(other_set)
+    print('size: {}'.format(new_set.hash_set.size))
 
 
 if __name__ == '__main__':
--- sets_test.py
+++ sets_test.py
@@ -73,10 +73,57 @@ class TestSets(unittest.TestCase):
         with self.assertRaises(KeyError):
             set.remove('A')  # Key does not exist
 
-        def union(self):
-            set = Set()
+    def test_union(self):
+        s = Set(['k', 'G', 'T'])
+        o_s = Set(['K', 'T', 'R'])
+        n_s = s.union(o_s)
 
+        self.assertCountEqual(n_s.hash_set.keys(), ['k', 'K', 'G', 'T', 'R'])
+        assert n_s.hash_set.size == 5
 
+        s_2 = Set(['m', 'M', 'l', 'Y', 'O'])
+        o_s_2 = Set(['P', 'M', 'l', 'Y'])
+        n_s_2 = s_2.union(o_s_2)
+
+        self.assertCountEqual(n_s_2.hash_set.keys(), ['m', 'M', 'l', 'Y', 'O', 'P'])
+        assert n_s_2.hash_set.size == 6
+
+        s_3 = Set([5, 4, 5, 6, 0])
+        o_s_3 = Set([6, 7, 7, 4])
+        n_s_3 = s_3.union(o_s_3)
+
+        self.assertCountEqual(n_s_3.hash_set.keys(), [5, 4, 6, 0, 7])
+        assert n_s_3.hash_set.size == 5
+
+    def test_intersection(self):
+        s = Set(['T', 'Y', 'I', 'P'])
+        o_s = Set(['P', 'I', 'T'])
+        n_s = s.intersection(o_s)
+
+        self.assertCountEqual(n_s.hash_set.keys(), ['T', 'I', 'P'])
+        assert n_s.hash_set.size == 3
+
+        s_2 = Set([2, 0, 0, 8])
+        o_s_2 = Set([2, 8, 0])
+        n_s_2 = s_2.intersection(o_s_2)
+
+        self.assertCountEqual(n_s_2.hash_set.keys(), [2, 0, 8])
+        assert n_s_2.hash_set.size == 3
+
+    def test_difference(self):
+        s = Set(['T', 'Y', 'O', 'P'])
+        o_s = Set(['T', 'R', 'V', 'O'])
+        n_s = s.difference(o_s)
+
+        self.assertCountEqual(n_s.hash_set.keys(), ['R', 'V'])
+        assert n_s.hash_set.size == 2
+
+        s_2 = Set([1, 3, 5, 6])
+        o_s_2 = Set([1, 6, 2, 4, 5])
+        n_s_2 = s_2.difference(o_s_2)
+
+        self.assertCountEqual(n_s_2.hash_set.keys(), [2, 4])
+        assert n_s_2.hash_set.size == 2
 
 if __name__ == '__main__':
     unittest.main()
