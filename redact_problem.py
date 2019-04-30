8f8594f2851a1fab1dffd9b48b8aa0f69bfd2ce3
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-04-29 23:32:53 -0700

    [add] implemented ionterview question

--- redact_problem.py
+++ redact_problem.py
@@ -2,11 +2,14 @@ from linkedlist import LinkedList
 
 
 def redact_words(words, banned_words):
-    word_list = [LinkedList() for word in words]
+    word_list = LinkedList(words)
+
 
     for word in banned_words:
-
-
+        found_word = word_list.find(lambda werd: werd == word)
+        if found_word is not None:
+            word_list.delete(found_word)
+    return word_list
 words = ['A', 'B', 'C', 'D', 'E']
 banned_words = ['B', 'D', 'E']
 print(redact_words(words, banned_words))
