7b2dc312b6ba9e164fdf23be57034ea3df6fefbb
Maximo Hinojosa <maximohinojosa13@gmail.com>
2019-05-03 01:54:53 -0700

    [add] implemented post order method

--- binarytree.py
+++ binarytree.py
@@ -294,12 +294,15 @@ class BinarySearchTree(object):
         Start at the given node and visit each node with the given function.
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
-        pass
+
         # TODO: Traverse left subtree, if it exists
-
+        if node.left:
+            self._traverse_post_order_recursive(node.left, visit)
         # TODO: Traverse right subtree, if it exists
-
+        if node.right:
+            self._traverse_post_order_recursive(node.right, visit)
         # TODO: Visit this node's data with given function
+        visit(node.data)
 
 
     def _traverse_post_order_iterative(self, node, visit):
